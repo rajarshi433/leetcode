6037d27a73090aa73ece5f15b2fc665addec38fc
Rajarshi Manna <mannarajarshi65@gmail.com>
2023-08-11 20:46:10 +0530

    Added solution - LeetHub

---
+++ Maximum of all subarrays of size k - GFG/maximum-of-all-subarrays-of-size-k.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find maximum of each subarray of size k.
+    def max_of_subarrays(self, nums, n,k):
+        
+        #code here
+        dq = deque()
+        ans = []
+
+        for i in range(n):
+            if dq and dq[0] <= (i - k):
+                dq.popleft()
+
+            while dq and nums[dq[-1]] < nums[i]:
+                dq.pop()
+
+            dq.append(i)
+
+            if i >= k - 1:
+                ans.append(nums[dq[0]])
+
+        return ans
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+from collections import deque
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n,k = map(int,input().strip().split())
+        arr = list(map(int,input().strip().split()))
+        ob=Solution()
+        res = ob.max_of_subarrays(arr,n,k)
+        for i in range (len (res)):
+            print (res[i], end = " ")
+        print()
+# } Driver Code Ends
